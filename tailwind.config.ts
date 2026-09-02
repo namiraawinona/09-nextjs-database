1f458e7bc0ed9a592decad3c6954771df0ff1169
samuderapang <samuderapanggabean@gmail.com>
2026-09-02 09:00:20 +0700

    W09: Jawaban soal 6

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        '13': 'repeat(13, minmax(0, 1fr))',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
